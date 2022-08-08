22d64ec8e799ddae08151e0d70939f1286df6004
kumuda <kumudapriya.sanka@gmail.com>
2022-08-08 15:16:59 +0530

    Add private function with named return var
    Signed-off-by: kumuda <kumudapriya.sanka@gmail.com>

--- LearnGoWithTests/helloWorld/hello.go
+++ LearnGoWithTests/helloWorld/hello.go
@@ -4,7 +4,7 @@ import "fmt"
 
 const spanish = "Spanish"
 const french = "French"
-const helloPrefix = "Hello "
+const englishPrefix = "Hello "
 const spanishPrefix = "Hola "
 const frenchPrefix = "Bonjour "
 
@@ -13,15 +13,21 @@ func Hello(name, language string) string {
 		name = "World"
 	}
 
-	prefix := helloPrefix
+	return greetingPrefix(language) + name
+}
+
+// private functions start with lowercase letter
+// public functions start with uppercase letter
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case spanish:
 		prefix = spanishPrefix
 	case french:
 		prefix = frenchPrefix
+	default:
+		prefix = englishPrefix
 	}
-
-	return prefix + name
+	return
 }
 
 func main() {
