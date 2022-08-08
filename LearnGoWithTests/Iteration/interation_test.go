7a361569b104b0b844321d4afebf19adb36e3be1
kumuda <kumudapriya.sanka@gmail.com>
2022-08-08 16:23:14 +0530

    [Iteration] : Introduce For loop
    Signed-off-by: kumuda <kumudapriya.sanka@gmail.com>

---
+++ LearnGoWithTests/Iteration/interation_test.go
@@ -0,0 +1,12 @@
+package iteration
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	got := Repeat("a")
+	want := "aaaaa"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)		
+	}
+}
---
+++ LearnGoWithTests/Iteration/iteration.go
@@ -0,0 +1,9 @@
+package iteration
+
+func Repeat(character string) (repeated string) {
+	for i := 0; i < 5; i++ {
+		repeated += character
+	}
+	return
+}
+
