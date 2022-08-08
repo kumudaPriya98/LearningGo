ff214a791f68a2a2dbbddc48eaef102d7a422500
kumuda <kumudapriya.sanka@gmail.com>
2022-08-08 17:05:35 +0530

    [Arrays] Introduce Slices => Collection of variable size
    Signed-off-by: kumuda <kumudapriya.sanka@gmail.com>

--- LearnGoWithTests/arrays/sum.go
+++ LearnGoWithTests/arrays/sum.go
@@ -1,6 +1,15 @@
 package main
 
-func Sum(numbers [5]int) (sum int) {
+func Sum(numbers []int) (sum int) {
+	// index and value
+	// Using blank identifier to ignore index here
+	for _, number := range numbers {
+		sum += number
+	}
+	return
+}
+
+func Sum5(numbers [5]int) (sum int) {
 	// index and value
 	// Using blank identifier to ignore index here
 	for _, number := range numbers {
--- LearnGoWithTests/arrays/sum_test.go
+++ LearnGoWithTests/arrays/sum_test.go
@@ -3,12 +3,27 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+	t.Run("Test the Sum for Array of size 5", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
 
-	if got != want {
-		t.Errorf("got '%d' want '%d', Nums : %v", got, want, numbers)
-	}
+		got := Sum5(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got '%d' want '%d', Nums : %v", got, want, numbers)
+		}
+	})
+
+	t.Run("Test the Sum for Slices", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8}
+
+		got := Sum(numbers)
+		want := 36
+
+		if got != want {
+			t.Errorf("got '%d' want '%d', Nums : %v", got, want, numbers)
+		}
+	})
+
 }
