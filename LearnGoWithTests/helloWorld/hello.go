a27fadc7c42dae09536134439da8038e6ab9b6f7
kumuda <kumudapriya.sanka@gmail.com>
2022-08-08 14:50:24 +0530

    Add test helper method to avoid code duplication
    Signed-off-by: kumuda <kumudapriya.sanka@gmail.com>

--- LearnGoWithTests/helloWorld/hello.go
+++ LearnGoWithTests/helloWorld/hello.go
@@ -5,6 +5,9 @@ import "fmt"
 const helloPrefix = "Hello "
 
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return helloPrefix + name
 }
 
--- LearnGoWithTests/helloWorld/hello_test.go
+++ LearnGoWithTests/helloWorld/hello_test.go
@@ -7,17 +7,25 @@ func TestHello(t *testing.T) {
 		got := Hello("Kumuda")
 		want := "Hello Kumuda"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertMessage(t, got, want)
 	})
 
 	t.Run("Saying Hello by default", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertMessage(t, got, want)
 	})
+}
+
+// Both *testing.T and *testing.B accept testing.TB
+func assertMessage(t testing.TB, got, want string) {
+	// the following declaration tell the test suite that, the current method is a helper method
+	// So, when the test fails, it prints the actual function line no, instead of current line no
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q wat %q", got, want)
+	}
+
 }
