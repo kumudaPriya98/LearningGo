a4c11c71271e89d481364046184eacf7e91ba130
kumuda <kumudapriya.sanka@gmail.com>
2022-08-08 14:41:41 +0530

    Add subtests
    Signed-off-by: kumuda <kumudapriya.sanka@gmail.com>

--- LearnGoWithTests/helloWorld/hello.go
+++ LearnGoWithTests/helloWorld/hello.go
@@ -2,8 +2,10 @@ package main
 
 import "fmt"
 
+const helloPrefix = "Hello "
+
 func Hello(name string) string {
-	return "Hello " + name
+	return helloPrefix + name
 }
 
 func main() {
--- LearnGoWithTests/helloWorld/hello_test.go
+++ LearnGoWithTests/helloWorld/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Kumuda")
-	want := "Hello Kumuda"
+	t.Run("Saying hello to myself", func(t *testing.T) {
+		got := Hello("Kumuda")
+		want := "Hello Kumuda"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("Saying Hello by default", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
