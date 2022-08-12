5e0c766af4a2d43db8212504a87dfde77000e50f
kumuda <kumudapriya.sanka@gmail.com>
2022-08-12 17:22:59 +0530

    [PointersAndError] Replace 'float64' with new type 'Bitcoin' and define String() for Bitcoin
    Signed-off-by: kumuda <kumudapriya.sanka@gmail.com>

--- LearnGoWithTests/PointersAndErrors/wallet.go
+++ LearnGoWithTests/PointersAndErrors/wallet.go
@@ -2,16 +2,22 @@ package main
 
 import "fmt"
 
+type Bitcoin float64
+
 type Wallet struct {
-	balance float64
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount float64) {
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%g Bitcoin", b)
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address in Deposit is %v \n", &(w.balance))
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() float64 {
+func (w *Wallet) Balance() Bitcoin {
 	fmt.Printf("address in Balance is %v \n", &(w.balance))
 	return w.balance
 }
--- LearnGoWithTests/PointersAndErrors/wallet_test.go
+++ LearnGoWithTests/PointersAndErrors/wallet_test.go
@@ -6,14 +6,14 @@ import (
 )
 
 func TestDeposit(t *testing.T) {
-	wallet := Wallet{0.0}
-	wallet.Deposit(10.0)
+	wallet := Wallet{Bitcoin(0.0)}
+	wallet.Deposit(Bitcoin(10.0))
 
 	fmt.Printf("address in Test is %v \n", &wallet.balance)
 	got := wallet.Balance()
-	want := 10.0
+	want := Bitcoin(12.0)
 
 	if got != want {
-		t.Errorf("got %g want %g", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
