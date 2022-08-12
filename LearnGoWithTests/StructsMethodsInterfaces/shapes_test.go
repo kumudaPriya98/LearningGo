238bc75b0a54fb58487d9a3eb927114228ff4404
kumuda <kumudapriya.sanka@gmail.com>
2022-08-12 14:33:25 +0530

    []StructsMethodsInterfaces] use t.Run in the table driven tests and name the tests
    You can run individual test by using "go test -run TestArea/Rectangle"
    By naming the test, it is easier to identify, which test is failing
    
    Signed-off-by: kumuda <kumudapriya.sanka@gmail.com>

--- LearnGoWithTests/StructsMethodsInterfaces/shapes_test.go
+++ LearnGoWithTests/StructsMethodsInterfaces/shapes_test.go
@@ -19,19 +19,21 @@ type Shape interface {
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{shape: Rectangle{Width: 10.0, Height: 10.0}, want: 100.0},
-		{shape: Circle{Radius: 10.0}, want: 314.1592653589793},
-		{shape: Triangle{Height: 10.0, Base: 10.0}, want: 50.0},
+		{name: "Rectangle", shape: Rectangle{Width: 10.0, Height: 10.0}, want: 100.0},
+		{name: "Circle", shape: Circle{Radius: 10.0}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Height: 10.0, Base: 10.0}, want: 50.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("got %g want %g", got, tt.want)
+			}
+		})
 	}
 }
