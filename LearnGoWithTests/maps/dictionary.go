0fbc82ce9462dc979e5e1b61c7dec6d958426aa7
kumuda <kumudapriya.sanka@gmail.com>
2022-08-14 15:39:10 +0530

    [Maps] Introduce "Map is a pointer to runtime.hmap structure" concept
    Otherwise we would have used d *Dictionary in the Add method
    
    Signed-off-by: kumuda <kumudapriya.sanka@gmail.com>

--- LearnGoWithTests/maps/dictionary.go
+++ LearnGoWithTests/maps/dictionary.go
@@ -6,6 +6,13 @@ type Dictionary map[string]string
 
 var ErrNoKey = errors.New("Key not present")
 
+// the change in the d will be reflected in the callee dictionary instance
+// As Map value is "pointer to a runtime.hmap" structure
+// When Add is called, the pointer is copied and modified
+func (d Dictionary) Add(key string, value string) {
+	d[key] = value
+}
+
 func (d Dictionary) Search(key string) (string, error) {
 	value, found := d[key]
 	if !found {
--- LearnGoWithTests/maps/dictionary_test.go
+++ LearnGoWithTests/maps/dictionary_test.go
@@ -2,25 +2,54 @@ package main
 
 import "testing"
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "Sample Value")
+
+	want := "Sample Value"
+	got, err := dictionary.Search("test")
+
+	assertNoError(t, err)
+	assertStrings(t, got, want)
+}
+
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "Sample Value"}
 
 	t.Run("known String", func(t *testing.T) {
-		got, _ := dictionary.Search("test")
+		got, err := dictionary.Search("test")
 		want := "Sample Value"
+
+		assertNoError(t, err)
 		assertStrings(t, got, want)
 	})
 
 	t.Run("unknown String", func(t *testing.T) {
-		_, err := dictionary.Search("unknown")
-		want := ErrNoKey.Error()
+		value, err := dictionary.Search("unknown")
 
-		if err == nil {
-			t.Fatal("Expected error but got no error")
-		}
-
-		assertStrings(t, err.Error(), want)
+		assertError(t, err, ErrNoKey)
+		assertStrings(t, value, "")
 	})
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+
+	if got != nil {
+		t.Errorf("expected no error, but got %q", got)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got == nil {
+		t.Fatal("Expected error but got no error")
+	}
+
+	if got != want {
+		t.Errorf("got error %q, want error %q", got, want)
+	}
 }
 
 func assertStrings(t testing.TB, got, want string) {
